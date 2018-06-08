4d05ec2c9d45f92cf0c13f47453a2e5ca33d670f
jennifermliu <jenniferliu2018@u.northwestern.edu>
2018-06-07 23:43:18 -0500

    end game page

--- src/admin/EndGameController.java
+++ src/admin/EndGameController.java
@@ -1,4 +1,26 @@
 package tsuro.admin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import tsuro.parser.Parser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class EndGameController {
+
+    @FXML
+    private ImageView boardImageView;
+
+    @FXML
+    private Label winnerList;
+
+    public void initialize() throws Exception {
+        boardImageView.setImage(new Image(new FileInputStream("image/board/board.png")));
+        String temp = App.socket.readInputFromServer();
+        winnerList.setText(temp);
+    }
 }
