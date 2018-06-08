886c16fc27372274206da80fef2ec76dae443aa1
jennifermliu <jenniferliu2018@u.northwestern.edu>
2018-06-07 22:49:48 -0500

    allow choosing number of players to start game

--- src/main/Tsuro.java
+++ src/main/Tsuro.java
@@ -8,7 +8,7 @@ public class Tsuro {
         // start a local host for networked tournament
         Server server = Server.getInstance();
         try {
-            server.startGame();
+            server.startGame(1,7,0);
         } catch (Exception e) {
             e.printStackTrace();
         }
